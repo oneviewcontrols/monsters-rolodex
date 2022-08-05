35051a69cec115814d03d7b91e39daab31085a45
Andy SanClemente <andysanclemente@gmail.com>
2022-08-05 09:18:20 -0400

    Section 3.31

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: { firstName: 'ansancle', lastName: 'fooo'},
+      company: 'ACME'
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +19,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hi {this.state.name.firstName}</p>
+        <button onClick={() => {
+          this.setState(
+            () => {
+            return {
+              name : {firstName: 'Bob', lastName: 'Deedle'}
+            };
+          },
+          () => {
+            console.log(this.state);
+          }
+          )
+        }}
+        >Click Me</button>
       </div>
     );
   }
