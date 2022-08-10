4fd9c0dfa9356c906a1203c9593e444df0d173e4
Andy SanClemente <andysanclemente@gmail.com>
2022-08-09 21:17:36 -0400

    Section 3.50

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import './card-list.styles.css';
+import './card.styles.css';
 
 class CardList extends Component {
 
     render() {
         const {monsters} = this.props;
         return (
-            monsters.map((monster) => (
-                <h1 key={monster.id}>{monster.name}</h1>
-            ))
-        )
+            <div className='card-list'>
+            {monsters.map((monster) => {
+                const {name, email, id} = monster;
+                return (
+                <div className='card-container' key={id}>
+                    <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                    <h2>{name}</h2>
+                    <p>{monster.email}</p>
+                </div>
+            )})}
+        </div>)
     }
 }
 
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './search-box-styles.css';
+import './search-box.styles.css';
 
 class SearchBox extends Component {
 
