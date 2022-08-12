96e07141a7e09d963c1fd6b51de31620571cdf67
Andy SanClemente <andysanclemente@gmail.com>
2022-08-12 09:43:01 -0400

    Section 3.52
    Monsters Rolodex Completed

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import Card from './card.component';
+import Card from '../card/card.component';
 import './card-list.styles.css';
-import './card.styles.css';
 
 class CardList extends Component {
 
@@ -12,7 +11,7 @@ class CardList extends Component {
             {monsters.map((monster) => {
                 const {name, email, id} = monster;                
                 return (                    
-                    <Card monster={monster}/>
+                    <Card key={monster.id} monster={monster}/>
             )})}   
         </div>)
     }
