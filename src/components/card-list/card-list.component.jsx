4f907775c5e3390feb36acf2290b1894f3e9623d
Andy SanClemente <andysanclemente@gmail.com>
2022-08-12 09:27:47 -0400

    Section 3.50

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from './card.component';
 import './card-list.styles.css';
 import './card.styles.css';
 
@@ -9,14 +10,10 @@ class CardList extends Component {
         return (
             <div className='card-list'>
             {monsters.map((monster) => {
-                const {name, email, id} = monster;
-                return (
-                <div className='card-container' key={id}>
-                    <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
-                    <h2>{name}</h2>
-                    <p>{monster.email}</p>
-                </div>
-            )})}
+                const {name, email, id} = monster;                
+                return (                    
+                    <Card monster={monster}/>
+            )})}   
         </div>)
     }
 }
